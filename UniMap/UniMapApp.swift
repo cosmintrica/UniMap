fe5b22c3a2686a54914942f8b294d57e9a4dea16
Cosmin Trica <cosmintrricaa@gmail.com>
2025-08-12 00:11:10 +0300

    Initial Commit

---
+++ UniMap/UniMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniMapApp.swift
+//  UniMap
+//
+//  Created by Cosmin Trica on 12.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct UniMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniMapTests/UniMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniMapTests.swift
+//  UniMapTests
+//
+//  Created by Cosmin Trica on 12.08.2025.
+//
+
+import Testing
+@testable import UniMap
+
+struct UniMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
